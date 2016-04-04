9893eb9b16a51512b6a419ad927d0ccf956f9e6d
Tim Holy <tim.holy@gmail.com>
2016-04-04 07:53:51 -0500

    Introduce the index and stored wrappers
    These are probably a better alternative to couple(any, ...) and couple(all, ...)

--- src/vectors_couple.jl
+++ src/vectors_couple.jl
@@ -1,8 +1,8 @@
 # These are attempts to write code in a future iteration API
 
 function copy!(dest, src)
-    for (idest, isrc) in couple(any, dest, src)  # any should be the default
-        dest[idest] = src[isrc]
+    for (idest, s) in couple(index(dest), src)
+        dest[idest] = s
     end
     dest
 end
@@ -12,7 +12,7 @@ end
 # This implementation has the same problem as reported in #15690.
 function vecdot(x::AbstractVector, y::AbstractVector)
     length(x) == length(y) || throw(DimensionMismatch("x and y are of different lengths!"))
-    citer = couple(all, x, y)  # all, because a*b = 0 unless both are nonzero
+    citer = couple(stored(x), stored(y))
     cstate = start(citer)
     done(state) && return dot(zero(eltype(x)), zero(eltype(y)))
     # Otherwise we avoid calling zero
@@ -35,12 +35,12 @@ end
 #
 # Here I punt on the "inference problem" and just indicate the output type
 # as the first argument.
-for (op, flag) in ((:+, :any), (:.*, :all))
+for op in (:+, :.*)
     @eval begin
         function ($op){Tout<:SparseVector}(::Type{Tout}, x::AbstractVector{Tx}, y::AbstractVector{Ty})
             length(x) == length(y) || throw(DimensionMismatch("x and y are of different lengths!"))
             z = Tout(length(x))  # doesn't exist yet, I think
-            for (ix, iy) in couple($flag, x, y)
+            for (ix, iy) in couple(index(x), index(y))
                 push!(z.nzind, Int(ix))
                 push!(z.nzval, $op(x[ix], y[iy]))
             end
